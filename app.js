1e25ea988332e7f80eb89a451aa81b0b813bf262
Mei Sun <meysunlee@gmail.com>
2021-02-16 18:18:22 +0200

    Fix

--- app.js
+++ app.js
@@ -6,27 +6,39 @@ app.use(express.json())
 app.use(cors())
 
 app.post('/api/analyze', (request, response) => {
-    const textLength = request.body['text'].length
-    const textRemoveSpace = request.body['text'].replace(/\s+/g, '')
-    const textLengthNoSpace = textRemoveSpace.length
-    const wordCount = request.body['text'].match(/\S+/g).length
-    const letterRemoveNumber = textRemoveSpace.replace(/[0-9]/g, '')
-    const letterRemoveSpecialChar = letterRemoveNumber.replace(/[^\w\s]/gi,'')
-    const letters = letterRemoveSpecialChar.split('').sort()
-    const letterCount = letters.reduce((acc, value) => ({
-        ...acc,
-        [value]: (acc[value] || 0) + 1
-    }), {})
-    const arryObj = Object.entries(letterCount).map((e) => ({ [e[0]]: e[1] }))
+    let responseBody
+    if (JSON.stringify(request.body) === JSON.stringify({})) {
+        responseBody = {
+            "textLength": {
+                "withSpaces": 0,
+                "withoutSpaces": 0
+            },
+            "wordCount": 0,
+            "characterCount": []
+        }
+    } else {
+        const textLength = request.body['text'].length
+        const textRemoveSpace = request.body['text'].replace(/\s+/g, '')
+        const textLengthNoSpace = textRemoveSpace.length
+        const wordCount = request.body['text'].match(/\S+/g).length
+        const letterRemoveNumber = textRemoveSpace.replace(/[0-9]/g, '')
+        const letterRemoveSpecialChar = letterRemoveNumber.replace(/[^\w\s]/gi,'')
+        const letters = letterRemoveSpecialChar.split('').sort()
+        const letterCount = letters.reduce((acc, value) => ({
+            ...acc,
+            [value]: (acc[value] || 0) + 1
+        }), {})
+        const arryObj = Object.entries(letterCount).map((e) => ({ [e[0]]: e[1] }))
+    
+    
+        responseBody = ({
+            textLength: { 'withSpaces': textLength, 'withoutSpaces': textLengthNoSpace },
+            wordCount: wordCount,
+            letterCount: arryObj
+        })
+    }
 
-
-    const newBody = ({
-        textLength: { 'withSpaces': textLength, 'withoutSpaces': textLengthNoSpace },
-        wordCount: wordCount,
-        letterCount: arryObj
-    })
-
-    response.json(newBody)
+    response.json(responseBody)
 })
 
 module.exports = app
